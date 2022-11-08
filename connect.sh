0579cb16e2ab2bc7367dc3fc0ccdee8aeb285d57
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-08 12:54:21 +0000

    Add Arch Linux's PKGBUILD & systemd service file

--- connect.sh
+++ connect.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-COMMAND_NAME="aws-vpn-client"
-OPENVPN_BIN="./build/openvpn-glibc"
-OPENVPN_CONF="./build/ovpn.conf"
-VPN_CLIENT_UP="./connect/vpn-client.up"
-VPN_CLIENT_DOWN="./connect/vpn-client.down"
+BASE_DIR=$(realpath "$(dirname "$0")")
+CMD_NAME="aws-vpn-client"
+
+CMD_BIN="$BASE_DIR/build/$CMD_NAME"
+OPENVPN_BIN="$BASE_DIR/build/openvpn-glibc"
+OPENVPN_CONF="$BASE_DIR/build/ovpn.conf"
+VPN_CLIENT_UP="$BASE_DIR/connect/vpn-client.up"
+VPN_CLIENT_DOWN="$BASE_DIR/connect/vpn-client.down"
 
 function parse_option_arg() {
   if [[ -n "${2-}" ]] && [[ ${2:0:1} != "-" ]]; then
@@ -18,6 +21,10 @@ function parse_option_arg() {
 function parse_args() {
   while (( "$#" )); do
     case "$1" in
+      --cmd)
+        CMD_BIN=$(parse_option_arg "$@")
+        shift 2
+        ;;
       --ovpn)
         OPENVPN_BIN=$(parse_option_arg "$@")
         shift 2
@@ -40,23 +47,36 @@ function parse_args() {
 
 function main() {
   parse_args "$@"
+  debug
+  connect
+}
 
-  "./build/$COMMAND_NAME" \
+function debug() {
+  echo "Connecting to AWS VPN using:"
+  echo "CMD_BIN=$CMD_BIN"
+  echo "OPENVPN_BIN=$OPENVPN_BIN"
+  echo "OPENVPN_CONF=$OPENVPN_CONF"
+  echo "VPN_CLIENT_UP=$VPN_CLIENT_UP"
+  echo "VPN_CLIENT_DOWN=$VPN_CLIENT_DOWN"
+}
+
+function connect() {
+  "$CMD_BIN" \
     -ovpn "$OPENVPN_BIN" \
     -config "$OPENVPN_CONF" \
     -verbose \
-    2>| "/tmp/$COMMAND_NAME.log" \
-    >| "/tmp/$COMMAND_NAME.saml"
+    2>| "/tmp/$CMD_NAME.log" \
+    >| "/tmp/$CMD_NAME.saml"
 
-  REMOTE_IP=$(grep 'Remote IP:' "/tmp/$COMMAND_NAME.log" | cut -d' ' -f5)
+  REMOTE_IP=$(grep 'Remote IP:' "/tmp/$CMD_NAME.log" | cut -d' ' -f5)
 
   sudo "$OPENVPN_BIN" \
     --config "$OPENVPN_CONF" \
     --remote "$REMOTE_IP" 443 \
     --up "$VPN_CLIENT_UP" \
     --down "$VPN_CLIENT_DOWN" \
-    --route-up "/usr/bin/env rm /tmp/$COMMAND_NAME.saml" \
-    --auth-user-pass /tmp/$COMMAND_NAME.saml
+    --route-up "/usr/bin/env rm /tmp/$CMD_NAME.saml" \
+    --auth-user-pass /tmp/$CMD_NAME.saml
 }
 
 main "$@"
